7f0ca3e17e7e41ddc2f583b4cac47eb44c2319e8
Ahmed Malick <ahmedmalik2k20@gmail.com>
2021-12-17 20:02:24 +0500

    Initial commit

---
+++ drum-kit/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("keydown", (e) => {
+  const keyName = e.key;
+  if (keyName.length === 1 && keyName.match(/[a-z]/i)) {
+    const audio = new Audio(`./audios/${keyName}.mp3`);
+    audio.currentTime = 0;
+    audio.play();
+  }
+});
