666d4edf7555bc7175216d80acf1346f67f51cb1
Ahmed Malik <ahmedmalik2k20@gmail.com>
2021-12-26 00:26:05 +0500

    done array cardio 2

---
+++ array-cardio-2/index.js
@@ -0,0 +1,37 @@
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
+
+const currentDate = new Date();
+
+// const some = people.some(
+//   (people) => currentDate.getFullYear() - people.year > 19
+// );
+
+// console.log(some);
+
+// const every = people.every(
+//   (people) => currentDate.getFullYear() - people.year > 19
+// );
+
+// console.log(every);
+
+// const find = comments.find((comment) => comment.id === 823423);
+
+// console.log(find);
+
+// const findIndex = comments.findIndex((comment) => comment.id === 823423);
+// comments.splice(findIndex, 1);
+
+// console.log(findIndex);
