0a8e714ccaf68ea7f0b9898e4faa239c1c8d25e5
Ahmed Malik <ahmedmalik2k20@gmail.com>
2022-01-23 02:03:45 +0500

    delete some folders

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,22 +1,21 @@
-window.addEventListener("keydown", (e) => {
-  const keyName = e.key;
-  if (keyName.length === 1 && keyName.match(/[a-z]/i)) {
-    const audio = new Audio(`./audios/${keyName}.mp3`);
-    audio.currentTime = 0;
-    audio.play();
-  }
-});
+const buttons = document.querySelectorAll("button")
 
-let buttons = document.querySelectorAll("button");
+document.addEventListener("keydown", (e) => {
+  const keyName = e.key.toLowerCase()
+  if (keyName.length === 1 && keyName.match(/[a-z]/i)) {
+    const audio = new Audio(`./audios/${keyName}.mp3`)
+    audio.currentTime = 0
+    audio.play()
+  }
+})
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const innerText = e.target.innerText;
+    const innerText = e.target.innerText.toLowerCase()
     if (innerText.length === 1 && innerText.match(/[a-z]/i)) {
-      const audio = new Audio(`./audios/${innerText.toLowerCase()}.mp3`);
-      console.log(audio);
-      audio.currentTime = 0;
-      audio.play();
+      const audio = new Audio(`./audios/${innerText}.mp3`)
+      audio.currentTime = 0
+      audio.play()
     }
-  });
-});
+  })
+})
